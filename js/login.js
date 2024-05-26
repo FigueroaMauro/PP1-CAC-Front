4fa4586df263257a89bbe275161a3b7658f40ea2
mauro <maurodev.tech@gmail.com>
2024-05-26 15:08:21 -0300

    validate

---
+++ js/login.js
@@ -0,0 +1,33 @@
+let emailError = document.querySelector("#error-email");
+let passError = document.querySelector("#error-pass");
+
+const btnLogin = document.querySelector("#btn-login");
+btnLogin.addEventListener("click", ()=>{
+    dataValidate();
+})
+
+const dataValidate = ()=>{
+    cleanError();
+    emailValidate();
+    passValidate(); 
+}
+
+
+const  emailValidate= ()=>{
+    let inpEmail = document.querySelector("#inp-email");
+    if(inpEmail.value.trim() == ""){
+        emailError.innerHTML = "Este campo es obligatorio y no debe estar vacío"
+    }
+}
+
+const  passValidate= ()=>{
+    let inpPass = document.querySelector("#inp-pass");
+    if(inpPass.value.trim() == ""){
+        passError.innerHTML = "Este campo es obligatorio y no debe estar vacío"
+    }
+}
+
+const cleanError = ()=>{
+    emailError.innerHTML = "";
+    passError.innerHTML = "";
+}
